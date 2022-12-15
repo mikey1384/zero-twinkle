de0c9ca3b147f6e035b7cf0440c6bf8cec8c52e3
mikey1384 <mikey1384@gmail.com>
2022-12-15 20:50:07 +0700

    minor fix

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ const { checkAndRespondToProfileMessages } = require("./model/response");
 const { summarizeMemories } = require("./model/memory");
 
 setInterval(checkAndRespondToProfileMessages, 60 * 1000);
-setInterval(summarizeMemories, 60 * 1000);
+setInterval(summarizeMemories, 20 * 1000);
