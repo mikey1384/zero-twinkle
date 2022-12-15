20cc4335c1727787b96852a2d9f9cdb8ba432647
mikey1384 <mikey1384@gmail.com>
2022-12-15 21:20:54 +0700

    minor fix

--- model/memory.js
+++ model/memory.js
@@ -44,7 +44,7 @@ async function summarizeMemories() {
     const { prompt, response } = row;
     const isSummarizedPromptRes = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `When you enter a prompt you said to me earlier, I am going to summarize your prompt/question to 3 or fewer sentences, and present it in this format - Summary: [summary goes here]. Enter your prompt here: \n\n"${prompt}"\n\n\n`,
+      prompt: `When you enter a prompt you said to me earlier, If it's a short question, I'll simply repeat the question. Otherwise I will summarize it to 3 or fewer sentences, and present it in this format - Summary: [summary goes here]. Enter your prompt here: \n\n"${prompt}"\n\n\n`,
       temperature: 0.7,
       max_tokens: 3000,
       top_p: 1,
