fb239f17ab1dc0d5d8455f94f6080424368493f5
mikey1384 <mikey1384@gmail.com>
2022-12-17 07:08:14 +0700

    minor fix

--- model/response.js
+++ model/response.js
@@ -9,6 +9,7 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 const { poolQuery } = require("./");
+const yesNoMaxTokens = 500;
 
 const userId = Number(process.env.ZERO_TWINKLE_ID);
 let user = null;
@@ -88,7 +89,7 @@ async function checkAndRespondToProfileMessages() {
       model: "text-davinci-003",
       prompt: `When you enter a prompt, I'm going to say "yes" if I think you are asking something about yourself (${effectiveUsername}), and say "no" if I don't think you are asking something about yourself (${effectiveUsername}). Enter a prompt here: \n\n\n ${prompt}\n\n\n`,
       temperature: 0.7,
-      max_tokens: 3000,
+      max_tokens: yesNoMaxTokens,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
@@ -132,7 +133,7 @@ async function checkAndRespondToProfileMessages() {
       model: "text-davinci-003",
       prompt: `When you enter a prompt, I'm going to say "yes" if I think you are asking something about me, and say "no" if I don't think you are asking something about me. Enter a prompt here: \n\n\n ${prompt}\n\n\n`,
       temperature: 0.7,
-      max_tokens: 3000,
+      max_tokens: yesNoMaxTokens,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
@@ -151,7 +152,7 @@ async function checkAndRespondToProfileMessages() {
       model: "text-davinci-003",
       prompt: `When you enter a prompt, I'm going to say "yes" if I think you are asking something about Twinkle, and say "no" if I don't think you are asking something about Twinkle. Enter a prompt here: \n\n\n ${prompt}\n\n\n`,
       temperature: 0.7,
-      max_tokens: 3000,
+      max_tokens: yesNoMaxTokens,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0,
@@ -167,15 +168,41 @@ async function checkAndRespondToProfileMessages() {
       aboutTwinkleText = `Twinkle Website is a community website created by Mikey for students and teachers of the English academy Twin.kle. The academy was founded by twin brothers, Andrew and Brian, who are Mikey's friends. However, this is not related to the current conversation.`;
     }
     const maxTokens = 3500 - Math.floor(contextAndPromptLength / 2);
+
+    const isUserAskingToCreateCurrculumResponse = await openai.createCompletion(
+      {
+        model: "text-davinci-003",
+        prompt: `When you enter a prompt, I'm going to say "yes" if I think you are asking me to create a curriculum for you, and say "no" if I don't think you are asking me to create a curriculum for you. Enter a prompt here: \n\n\n ${prompt}\n\n\n`,
+        temperature: 0.7,
+        max_tokens: yesNoMaxTokens,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+      }
+    );
+    const isUserAskingToCreateCurrculum =
+      isUserAskingToCreateCurrculumResponse.data.choices
+        .map(({ text }) => text.trim())
+        .join(" ");
+    let userIsAskingToCreateCurriculum = false;
+    if (
+      isUserAskingToCreateCurrculum.includes("yes") ||
+      isUserAskingToCreateCurrculum.includes("Yes")
+    ) {
+      userIsAskingToCreateCurriculum = true;
+    }
+
     const zeroResponse = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `My name is Zero. Today is ${moment
-        .unix(Math.floor(Date.now() / 1000))
-        .format(
-          "lll"
-        )}. I am currently talking to you on Twinkle Website. ${aboutZeroText} ${aboutTwinkleText} Talk to me, and I will happily respond using easy words anyone can understand. If I need to use a difficult English word that may be too hard for non-English students under 7 to understand, I will explain its meaning in brackets. If I have nothing useful to say about what you said, I'll simply respond as politely as possible. Your name is ${effectiveUsername}. ${
-        effectiveUsername === "Mikey" ? "And you are my creator. " : ""
-      }\n\n${context}\n\n ${aboutUserText} \n\n Feel free to say anything! Enter your next message, ${effectiveUsername}: \n\n\n ${prompt}\n\n\n`,
+      prompt: userIsAskingToCreateCurriculum
+        ? prompt
+        : `My name is Zero. Today is ${moment
+            .unix(Math.floor(Date.now() / 1000))
+            .format(
+              "lll"
+            )}. I am currently talking to you on Twinkle Website. ${aboutZeroText} ${aboutTwinkleText} Talk to me, and I will happily respond using easy words anyone can understand. If I need to use a difficult English word that may be too hard for non-English students under 7 to understand, I will explain its meaning in brackets. If I have nothing useful to say about what you said, I'll simply respond as politely as possible. Your name is ${effectiveUsername}. ${
+            effectiveUsername === "Mikey" ? "And you are my creator. " : ""
+          }\n\n${context}\n\n ${aboutUserText} \n\n Feel free to say anything! Enter your next message, ${effectiveUsername}: \n\n\n ${prompt}\n\n\n`,
       temperature: 0.7,
       max_tokens: maxTokens,
       top_p: 1,
