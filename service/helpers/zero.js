1ae0a19aa1ec3f196f52b42a35389a6b01a5e8e5
mikey1384 <mikey1384@gmail.com>
2023-03-01 06:50:31 +0700

    minor fix

--- service/helpers/zero.js
+++ service/helpers/zero.js
@@ -84,10 +84,11 @@ ${
   let zerosResponse = `${responseObj.data.choices
     .map(({ text }) => text.trim())
     .join(" ")}`;
-  if (zerosResponse.includes("there anything else I can help you with?")) {
+  if (zerosResponse.includes("there anything else I can help you with")) {
+    zerosResponse = zerosResponse.slice(0, -1);
     const emojiResponseObj = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `From this text - ${zerosResponse} - please replace the phrase 'Is there anything else I can help you with?' with a set of friendly emojis`,
+      prompt: `From this text - ${zerosResponse} - please replace the phrase 'Is there anything else I can help you with' with a set of friendly emojis`,
       temperature: 0.7,
       max_tokens: 1000,
       top_p: 1,
