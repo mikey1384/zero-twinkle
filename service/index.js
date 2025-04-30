ca1669f307c520c724854e45823085764f9f5596
mikey1384 <mikey1384@gmail.com>
2025-04-30 10:16:16 +0700

    remove hint

--- service/index.js
+++ service/index.js
@@ -1,10 +1,8 @@
 const { tagVideosToPlaylist, setPlaylistRewardLevel } = require("./playlist");
 const { checkAndTriggerRewardCard } = require("./reward");
-const { checkAndUpdateHints } = require("./hint.js");
 
 module.exports = {
   tagVideosToPlaylist,
   setPlaylistRewardLevel,
   checkAndTriggerRewardCard,
-  checkAndUpdateHints,
 };
