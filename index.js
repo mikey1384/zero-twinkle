ee2765bf4d22f1654c0b35e2b6647d5999157ec5
mikey1384 <mikey1384@gmail.com>
2025-04-30 10:19:35 +0700

    remove updateHint

--- index.js
+++ index.js
@@ -4,15 +4,12 @@ const {
   tagVideosToPlaylist,
   setPlaylistRewardLevel,
   checkAndTriggerRewardCard,
-  checkAndUpdateHints,
 } = require("./service");
 
 const tagVideosToPlaylistInterval = 60;
 const setPlaylistRewardLevelInterval = 60;
 const checkRewardCardInterval = 30;
-const checkAndUpdateHintsInterval = 1;
 
 setInterval(tagVideosToPlaylist, tagVideosToPlaylistInterval * 1000);
 setInterval(setPlaylistRewardLevel, setPlaylistRewardLevelInterval * 1000);
 setInterval(checkAndTriggerRewardCard, checkRewardCardInterval * 1000);
-setInterval(checkAndUpdateHints, checkAndUpdateHintsInterval * 1000);
